2124890ac22fc4320d2978e1642df6bfe78f2065
Sean Dempsey <dempsey.sean@outlook.com>
2024-01-27 15:20:43 -0800

    Update for RandomizerCore 2.0

--- MoreLocations/Rando/LogicPatcher.cs
+++ MoreLocations/Rando/LogicPatcher.cs
@@ -1,4 +1,5 @@
 ﻿using ItemChanger;
+using RandomizerCore.Json;
 using RandomizerCore.Logic;
 using RandomizerMod.RC;
 using RandomizerMod.Settings;
@@ -21,8 +22,7 @@ namespace MoreLocations.Rando
                 return;
             }
 
-            AddTermsAndItems(lmb);
-            AddLocationLogic(lmb);
+            AddLogicObjects(lmb);
         }
 
         private static void PatchLogic(GenerationSettings gs, LogicManagerBuilder lmb)
@@ -35,31 +35,30 @@ namespace MoreLocations.Rando
             OverrideLemmUsages(lmb);
         }
 
-        private static void AddTermsAndItems(LogicManagerBuilder lmb)
+        private static void AddLogicObjects(LogicManagerBuilder lmb)
         {
+            JsonLogicFormat fmt = new();
+
             using Stream t = typeof(LogicPatcher).Assembly.GetManifestResourceStream("MoreLocations.Resources.Logic.terms.json");
-            lmb.DeserializeJson(LogicManagerBuilder.JsonType.Terms, t);
+            lmb.DeserializeFile(LogicFileType.Terms, fmt, t);
 
             using Stream i = typeof(LogicPatcher).Assembly.GetManifestResourceStream("MoreLocations.Resources.Logic.items.json");
-            lmb.DeserializeJson(LogicManagerBuilder.JsonType.Items, i);
-        }
+            lmb.DeserializeFile(LogicFileType.Items, fmt, i);
 
-        private static void AddLocationLogic(LogicManagerBuilder lmb)
-        {
             using Stream l = typeof(LogicPatcher).Assembly.GetManifestResourceStream("MoreLocations.Resources.Logic.locations.json");
-            lmb.DeserializeJson(LogicManagerBuilder.JsonType.Locations, l);
+            lmb.DeserializeFile(LogicFileType.Locations, fmt, l);
         }
 
         private static void OverrideLemmUsages(LogicManagerBuilder lmb)
         {
             // even when lemm shop is not enabled, this should happen - we just need to define vanilla lemm placements in RB instead
-            string[] expensiveLocations = new string[] {
+            string[] expensiveLocations = [
                 LocationNames.Unbreakable_Greed,
                 LocationNames.Unbreakable_Heart,
                 LocationNames.Unbreakable_Strength,
                 LocationNames.Dash_Slash,
                 LocationNames.Vessel_Fragment_Basin
-            };
+            ];
             foreach (string expensiveLocation in expensiveLocations)
             {
                 lmb.DoSubst(new RawSubstDef(expensiveLocation, "Can_Visit_Lemm", TermNames.LARGEGEODEPOSIT));
